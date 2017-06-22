2eb61d8e0359092306a16c175fb01b226504ae84
geugon <geugon@gmail.com>
2017-06-21 20:14:39 -0700

    Add MazeRunner class

--- MazeRunner/mazeRunner.py
+++ MazeRunner/mazeRunner.py
@@ -13,35 +13,55 @@ import pygame
 
 SCREEN_SIZE = (640, 480)
 WHITE = (230, 230, 230)
+BLACK = (0, 0, 0)
 FPS = 30
 
-if __name__ == "__main__":
-    
-    # init
-    pygame.init()
-    screen=pygame.display.set_mode(SCREEN_SIZE)
 
-    # background
-    background = pygame.Surface(screen.get_size())
-    background.fill(WHITE)
-    screen.blit(background, (0,0))
+class MazeRunner():
 
-    # settings
-    clock = pygame.time.Clock()
-    clock.tick(FPS)
+    """ The game... """
 
-    # loop
-    running = True
-    while running:
+    def __init__(self):
+        pygame.init()
+        self.running = False
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        self.runtime = 0
         
+        # background
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background.fill(WHITE)
+
+        # settings
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont('freeansbold.ttf', 24)
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self._controlTick()
+            self._viewTick()
+    
+    def _controlTick(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    running = False
+                    self.running = False
 
+    def _viewTick(self):
+        self.runtime += self.clock.tick(FPS)
+        surface = self.font.render(str(self.runtime), True, BLACK)
+        self.screen.blit(self.background, (0,0))
+        self.screen.blit(surface, (50,50))
         pygame.display.update()
 
-    # clean up
-    pygame.quit()
+    def cleanup(self):
+        pygame.quit()
+
+
+
+if __name__ == "__main__":
+   mr = MazeRunner()
+   mr.run()
+   rm.cleanup()
