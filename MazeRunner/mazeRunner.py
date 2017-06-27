2ae657b5112fc6b03856276780622c0a0f0b5c40
geugon <geugon@gmail.com>
2017-06-27 06:37:10 -0700

    Add Settings class

--- MazeRunner/mazeRunner.py
+++ MazeRunner/mazeRunner.py
@@ -10,14 +10,20 @@ License:    Apache License 2.0
 """
 
 import pygame
+import json
+from ast import literal_eval
 
-SCREEN_SIZE = (640, 480)
-PLAYER_RADIUS = 10
-WHITE = (230, 230, 230)
-RED = (255, 0, 0)
-BLACK = (0, 0, 0)
-CLEAR = (0, 0, 0, 0)
-FPS = 30
+
+class Settings():
+    
+    """ Parses and stores settings from input json file """
+
+    def __init__(self, color_fname):
+        """ Input json must have single depth level """
+        with open(color_fname) as f:
+            data = json.load(f)
+        for k,v in data.items():
+            setattr(self, k, literal_eval(v))
 
 
 class PlayerSprite(pygame.sprite.Sprite):
@@ -26,9 +32,10 @@ class PlayerSprite(pygame.sprite.Sprite):
 
     def __init__(self):
         super(PlayerSprite, self).__init__()
-        self.image = pygame.Surface((PLAYER_RADIUS*2, PLAYER_RADIUS*2), pygame.SRCALPHA)
-        self.image.fill(CLEAR)
-        pygame.draw.circle(self.image, RED, (10, 10), PLAYER_RADIUS, 0)
+        r = settings.PLAYER_RADIUS
+        self.image = pygame.Surface((r*2, r*2), pygame.SRCALPHA)
+        self.image.fill(colors.CLEAR)
+        pygame.draw.circle(self.image, colors.RED, (r, r), r, 0)
         self.rect = self.image.get_rect()
         self.rect.center = (20, 100)
 
@@ -46,14 +53,14 @@ class MazeRunner():
     def __init__(self):
         pygame.init()
         self.running = False
-        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        self.screen = pygame.display.set_mode(settings.SCREEN_SIZE)
         self.runtime = 0
         
         # background
         self.background = pygame.Surface(self.screen.get_size())
-        self.background.fill(WHITE)
+        self.background.fill(colors.WHITE)
 
-        # settings
+        # setup
         self.clock = pygame.time.Clock()
         self.font = pygame.font.SysFont('freeansbold.ttf', 24)
 
@@ -89,8 +96,8 @@ class MazeRunner():
             self.playerSprite.move((1, 0))
 
     def _viewTick(self):
-        self.runtime += self.clock.tick(FPS)
-        surface = self.font.render(str(self.runtime), True, BLACK)
+        self.runtime += self.clock.tick(settings.FPS)
+        surface = self.font.render(str(self.runtime), True, colors.BLACK)
         self.screen.blit(self.background, (0,0))
         self.screen.blit(surface, (50,50))
 
@@ -103,6 +110,8 @@ class MazeRunner():
 
 
 if __name__ == "__main__":
+   colors = Settings('colors.json')
+   settings = Settings('settings.json')
    mr = MazeRunner()
    mr.run()
    mr.cleanup()
