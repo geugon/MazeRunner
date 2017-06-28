ed6fd3ab601eda8c7878c45c4daa36247cef4daf
geugon <geugon@gmail.com>
2017-06-28 06:45:04 -0700

    smoother movement

--- MazeRunner/mazeRunner.py
+++ MazeRunner/mazeRunner.py
@@ -14,6 +14,9 @@ import json
 from ast import literal_eval
 
 
+ROOT2 = 0.5**0.5
+
+
 class Settings():
     
     """ Parses and stores settings from input json file """
@@ -32,19 +35,59 @@ class PlayerSprite(pygame.sprite.Sprite):
 
     def __init__(self):
         super(PlayerSprite, self).__init__()
+
+        # setup image
         r = settings.PLAYER_RADIUS
         self.image = pygame.Surface((r*2, r*2), pygame.SRCALPHA)
         self.image.fill(colors.CLEAR)
         pygame.draw.circle(self.image, colors.RED, (r, r), r, 0)
         self.rect = self.image.get_rect()
-        self.rect.center = (20, 100)
 
-    def move(self, direction):
-        vel = 2
-        dx, dy = [tmp*vel for tmp in direction]
-        x, y = self.rect.center
-        self.rect.center = x+dx, y+dy
+        # setup movement
+        self._acc = settings.PLAYER_MAXSPEED*settings.PLAYER_DRAG
+        self._drag = settings.PLAYER_DRAG
+        self._pos = (20.0, 100.0) # unsnapped position
+        self._vel = (0.0, 0.0)
 
+        pos = tuple((int(x) for x in self._pos))
+        self.rect.center = pos
+
+    def set_direction(self, up, down, left, right):
+        if up and not down:
+            y = -1
+        elif down and not up:
+            y = 1
+        else:
+            y = 0
+
+        if left and not right:
+            x = -1
+        elif right and not left:
+            x = 1
+        else:
+            x = 0
+
+        if y==1 and x!=0:
+            y = ROOT2
+        if y==-1 and x!=0:
+            y = -ROOT2
+
+        if x==1 and y!=0:
+            x = ROOT2
+        if x==-1 and y!=0:
+            x = -ROOT2
+
+        self._dir = (x,y)
+
+    def update(self):
+        dx = (1.0-self._drag)*self._vel[0] + self._acc*self._dir[0]
+        dy = (1.0-self._drag)*self._vel[1] + self._acc*self._dir[1]
+
+        self._vel = (dx, dy)
+        self._pos = (self._pos[0]+self._vel[0],
+                     self._pos[1]+self._vel[1])
+        pos = tuple((int(x) for x in self._pos))
+        self.rect.center = pos
 
 class MazeRunner():
 
@@ -85,15 +128,10 @@ class MazeRunner():
                     self.running = False
 
         keys_pressed = pygame.key.get_pressed()
-
-        if keys_pressed[pygame.K_UP]:
-            self.playerSprite.move((0, -1))
-        if keys_pressed[pygame.K_DOWN]:
-            self.playerSprite.move((0, 1))
-        if keys_pressed[pygame.K_LEFT]:
-            self.playerSprite.move((-1, 0))
-        if keys_pressed[pygame.K_RIGHT]:
-            self.playerSprite.move((1, 0))
+        self.playerSprite.set_direction(keys_pressed[pygame.K_UP],
+                                        keys_pressed[pygame.K_DOWN],
+                                        keys_pressed[pygame.K_LEFT],
+                                        keys_pressed[pygame.K_RIGHT])
 
     def _viewTick(self):
         self.runtime += self.clock.tick(settings.FPS)
@@ -102,6 +140,7 @@ class MazeRunner():
         self.screen.blit(surface, (50,50))
 
         self.playerSpriteGroup.draw(self.screen)
+        self.playerSpriteGroup.update()
         pygame.display.update()
 
     def cleanup(self):
